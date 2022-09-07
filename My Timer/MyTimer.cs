a13baf111837a159e3a554604ec18d407f1936d8
24evgeniy03 <24evgeniy03@gmail.com>
2022-09-07 18:28:48 +0300

    Добавьте файлы проекта.

---
+++ My Timer/MyTimer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace My_Timer
+{
+    public static class MyTimer
+    {
+        public static void SetTimeout(int time, Action callback)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(time);
+                callback.Invoke();
+            });
+        }
+
+        public static void SetInterval(int time, Action callback, int count = 0)
+        {
+            var T = Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i <= count;)
+                {
+                    Thread.Sleep(time);
+                    callback.Invoke();
+                    if (count > 0)
+                    {
+                        i++;
+                        if (i == count) break;
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ My Timer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace My_Timer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyTimer.SetTimeout(3000, () => Console.WriteLine("Method 1"));
+            MyTimer.SetInterval(1000, () => Console.WriteLine("Method 2"), 4);
+            Console.ReadKey();
+        }
+    }
+}
